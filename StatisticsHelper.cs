c75e60f4715514129162a0e6fccc74570bc86fb0
Jakub Brochocki <s27343@pjwstk.edu.pl>
2026-03-11 20:17:55 +0100

    Add CalculateAverage method

--- StatisticsHelper.cs
+++ StatisticsHelper.cs
@@ -1,3 +1,19 @@
 public static class StatisticsHelper
 {
+    public static double CalculateAverage(int[] values)
+    {
+        if (values.Length == 0)
+        {
+            throw new ArgumentException("Tablica nie może być pusta.");
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            sum += values[i];
+        }
+
+        return (double)sum / values.Length;
+    }
 }
